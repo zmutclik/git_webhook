9a2c4e2ec2a5a2c36f53df9e8cbd4103db1fc72a
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2025-12-29 05:01:36 +0000

    -

--- build-image.sh
+++ build-image.sh
@@ -2,8 +2,7 @@
 
 docker build \
   --no-cache \
-  -t git_webhook:latest .
+  -t zmutclik/git_webhook:latest .
 
 # docker login
-docker tag git_webhook:latest zmutclik/git_webhook:latest
 docker push zmutclik/git_webhook:latest
